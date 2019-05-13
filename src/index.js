40f8099f311d80b4f7e3c49d44b3d19180e5e0f4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-13 12:00:30 -0700

    dog list with color

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-console.log('hi megs');
+import styles from './index.css';
+const rootDiv = document.getElementById('root');
+
+const dogList = document.createElement('ul');
+dogList.className = styles.dogs;
+
+const header = document.createElement('h1');
+header.textContent = 'my cute dogs';
+
+const dogs = ['spot', 'rover', 'bingo', 'joe'];
+
+rootDiv.appendChild(header);
+rootDiv.appendChild(dogList);
+
+dogs.forEach(dog => {
+  const dogLi = document.createElement('li');
+  dogLi.textContent = dog;
+  dogList.appendChild(dogLi);
+});
 
 
