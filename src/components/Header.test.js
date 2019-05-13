60806b88d6f48db76586ad28052000453b20768f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-13 14:11:25 -0700

    dog is displaying, working on test

---
+++ src/components/Header.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('Header component', () => {
+  it('matches a snapshot', () => {
+    const wrapper = shallow(<Header />);
+  })
+})
