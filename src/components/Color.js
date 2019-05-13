00f4378c6c1c0666baaa5a1423f3ea294964802b
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-13 15:27:48 -0700

    refactoring based on example

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,22 +1,37 @@
 import React from 'react';
 
+const hexFromColor = color => {
+  return `${color.toString(16).padStart(2, 0)}`.toUpperCase();
+};
+
+const hexFromRGB = (rgb) => {
+  return `#${hexFromColor(rgb.red)}${hexFromColor(rgb.green)}${hexFromColor(rgb.blue)}`;
+};
+
 export default function Color() {
-  const color = {
-    name: 'red',
-    hex: 'F00',
-    rgb: '255,0,0'
+  const name = 'Red';
+  const rgb = {
+    red: 255,
+    green: 0,
+    blue: 0
   };
+  const hex = hexFromRGB(rgb);
+  
 
   return (
     <dl>
       <dt>Name:</dt>
-      <dd>{color.name}</dd>
+      <dd>{name}</dd>
 
       <dt>Hex:</dt>
-      <dd>{color.hex}</dd>
+      <dd>{hex}</dd>
 
       <dt>RGB:</dt>
-      <dd>{color.rgb}</dd>
+      <dd>
+        <p>Red - {rgb.red}</p>
+        <p>Green - {rgb.green}</p>
+        <p>Blue - {rgb.blue}</p>
+      </dd>
     </dl>
   );
 }
